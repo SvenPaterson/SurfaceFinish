c9dc1c087d241c6e9800c116cd8bc0eeeefa6a38
SvenPaterson <stephenjgarden@icloud.com>
2022-12-03 17:38:22 -0500

    mixed noise created, on to filters next

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def sine_wave(x, frequency, amplitude, phase):
-    y = amplitude * np.sin(2 * np.pi * frequency * x + phase)
-    return y
-
+def generate_sine_wave(freq, sample_rate, length):
+    x = np.linspace(0, length, sample_rate * length, endpoint = False)
+    frequencies = x * freq
+    y = np.sin(frequencies * 2 * np.pi)
+    return x, y
 
 
 def main():
-    SAMPLE_RATE = 0.001
-    FREQ = 0.8
-
-    x = np.linspace(0, 5.6, int(1/SAMPLE_RATE))
-    signal = sine_wave(x, FREQ, 1, 0)
-    filter = gaussian_distribution(FREQ, SAMPLE_RATE)
-
-    # do 1st plotting here
-    plt.subplot(211)
-    plt.plot(x, filter)
-    plt.axis([-1, 1, 0, 1])
-    plt.title('Gaussian Filter')
-
-    # do 2nd plotting here
-    plt.subplot(212)
-    plt.plot(x, signal, 'b')
-    plt.axis([0, 5.6, -1.5, 1.5])
-    plt.title('Signal')
-
-    plt.tight_layout()
+    SAMPLE_RATE = 4000
+    LENGTH = 5
+    x, tone_a = generate_sine_wave(1, SAMPLE_RATE, LENGTH)
+    _, noise_tone = generate_sine_wave(15, SAMPLE_RATE, LENGTH)
+    mixed_tone = tone_a + noise_tone * 0.3
+    plt.plot(x, mixed_tone)
     plt.show()
 
 
